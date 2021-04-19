e764da7b89db6b034f3ed1729bfd436bb32499b6
shihongxins <shihongxins@163.com>
2021-04-20 00:18:06 +0800

    项目初始化（移动端必做的四件事）

--- src/main.js
+++ src/main.js
@@ -3,4 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'normalize.css'
+import './assets/style/base.scss'
+import './assets/style/border.css'
+
+import fastclick from 'fastclick'
+fastclick.attach(document.body)
+
 createApp(App).use(store).use(router).mount('#app')
